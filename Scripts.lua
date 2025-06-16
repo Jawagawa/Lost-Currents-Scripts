6f6454f8c9ac1c0c4624c151be1ce2f5d669869c
Jawagawa <maxdamn28@gmail.com>
2025-06-16 22:03:22 +0200

    We're so back!

--- Scripts.lua
+++ Scripts.lua
@@ -1,18 +1,18 @@
 -- Select classAdd commentMore actions
 local args = { [1] = "Builder" }
 game:GetService("ReplicatedStorage").SelectedClass:FireServer(unpack(args))
-
+-
 -- Return to lobby
 game:GetService("ReplicatedStorage").ReturnToLobby:FireServer()
-
+-
 -- Disable storms
 game:GetService("ReplicatedStorage").StormDisable:FireServer()
-
+-
 -- Infinite Yield (Admin Commands)More actions
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", true))()
-
+-
 -- Dex Explorer
 loadstring(game:HttpGet("https://raw.githubusercontent.com/BigBoyTimme/New.Loadstring.Scripts/refs/heads/main/Dex.Explorer", true))()
-
+-
 -- Fly script
 loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Fly.txt", true))()
