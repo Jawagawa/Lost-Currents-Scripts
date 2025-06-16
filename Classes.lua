ae773aa26c5347fb4e3d62e68d606c495bba9ddb
Jawagawa <maxdamn28@gmail.com>
2025-06-16 18:10:52 +0200

    Create Classes.lua

---
+++ Classes.lua
@@ -0,0 +1,9 @@
+# Classes:
+
+Bolt Licker, Builder, Businessman, Fisherman, Shark Hunter, Pirate, More on their wiki.
+
+local args = {
+    [1] = "Builder"
+}
+
+game:GetService("ReplicatedStorage").SelectedClass:FireServer(unpack(args))
